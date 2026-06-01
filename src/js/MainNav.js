7f3791a9e383573d765bd99ea9b79d257f9c28ca
James Mejia <s.jamesmejia@gmail.com>
2026-06-01 18:26:57 -0500

    Add mobile menu that resets on desktop.

--- src/js/MainNav.js
+++ src/js/MainNav.js
@@ -1,9 +1,11 @@
 const activeClass = "is-active";
 const siteNav = document.querySelector("[data-site-nav]");
+
 const siteMobileNavToggle = document.querySelector("[data-site-nav-toggle]");
 const sitePrimaryNav = siteNav.querySelector("[data-primary-nav]");
 const FOCUSABLE_ELEMENTS = "a, button";
 
+// Create an array from all focusable, active, elements.
 const focusableElements = [...siteNav.querySelectorAll(FOCUSABLE_ELEMENTS)]
   .filter((el) => !el.disabled)
   .filter((el) => !el.hidden);
@@ -11,43 +13,36 @@ const focusableElements = [...siteNav.querySelectorAll(FOCUSABLE_ELEMENTS)]
 const focusTrap = (event) => {
   const firstItem = focusableElements.at(0);
   const lastItem = focusableElements.at(-1);
-
-  if (
+  const tabForward =
     event.key === "Tab" &&
     document.activeElement === lastItem &&
-    !event.shiftKey // Needed because we check for reverse tab order later.
-  ) {
+    !event.shiftKey; // Avoid conflict with reverse tab order check.
+  const tabBackward = event.shiftKey && event.key === "Tab";
+
+  if (tabForward) {
     event.preventDefault(); // Tabs to 2nd item without this.
-    firstItem.tabIndex = 0;
     firstItem.focus();
   }
 
-  if (event.shiftKey && event.key === "Tab") {
-    // const activeItem = (element) => element === document.activeElement;
-    // const activeItemIndex = focusableElements.findIndex(activeItem);
-
-    // event.preventDefault();
-    // focusableElements[activeItemIndex - 1].focus();
-
-    if (document.activeElement === firstItem) {
-      event.preventDefault();
-      lastItem.focus();
-    }
+  if (tabBackward && firstItem === document.activeElement) {
+    event.preventDefault();
+    lastItem.focus();
   }
 
   if (event.key === "Escape") {
     siteMobileNavToggle.focus();
+    if (!siteNav.hidden) toggleMenu();
   }
 };
 
 const toggleMenu = () => {
-  let isExpanded = siteMobileNavToggle.ariaExpanded === "true";
+  const isExpanded = siteMobileNavToggle.ariaExpanded === "true";
   siteNav.hidden = isExpanded;
 
-  siteMobileNavToggle.setAttribute("aria-expanded", !isExpanded);
+  siteMobileNavToggle.ariaExpanded = `${!isExpanded}`;
 };
 
-const toggleCurrent = () => {
+const setCurrent = () => {
   const currentPath = window.location.pathname;
   const pathSegments = currentPath.split("/");
 
@@ -60,16 +55,59 @@ const toggleCurrent = () => {
     const activeItem = sitePrimaryNav.querySelector(
       `[href="/${pathSegments[1]}"]`,
     );
-    activeItem.classList.add(activeClass);
+    if (activeItem) activeItem.classList.add(activeClass);
   }
 };
 
-const init = () => {
-  // siteNav.hidden = true;
-  toggleCurrent();
+// Reset nav on desktop.
+// Matches `sm` (490px) breakpoint in `_variables`.
+const mqTablet = window.matchMedia("(width >= 30.625em)");
+
+const handleMqTablet = () => {
+  if (!mqTablet.matches) {
+    siteMobileNavToggle.ariaExpanded = "false";
+    siteMobileNavToggle.hidden = false;
+
+    siteNav.hidden = true;
+    addListeners();
+    return;
+  }
+
+  siteMobileNavToggle.ariaExpanded = "true";
+  siteMobileNavToggle.hidden = true;
+  siteNav.hidden = false;
+
+  removeListeners();
 };
 
-siteMobileNavToggle.addEventListener("click", toggleMenu);
-siteNav.addEventListener("keydown", focusTrap);
+// Close menu if user clicks outside of nav.
+const handleFocusOut = (event) => {
+  if (siteNav.hidden) return;
+  if (siteMobileNavToggle.contains(event.target)) return;
+  if (siteNav.contains(event.target)) return;
+
+  toggleMenu();
+};
+
+const addListeners = () => {
+  document.body.addEventListener("click", handleFocusOut);
+  siteMobileNavToggle.addEventListener("click", toggleMenu);
+  siteNav.addEventListener("keydown", focusTrap);
+};
+
+const removeListeners = () => {
+  document.body.removeEventListener("click", handleFocusOut);
+  siteMobileNavToggle.removeEventListener("click", toggleMenu);
+  siteNav.removeEventListener("keydown", focusTrap);
+};
+
+const init = () => {
+  if (!siteNav || !siteMobileNavToggle) return;
+
+  mqTablet.addEventListener("change", handleMqTablet);
+
+  handleMqTablet();
+  setCurrent();
+};
 
 export default init;
